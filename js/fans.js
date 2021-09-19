53d3c5e5f332834696e160464438e882587de2c4
halyna-m <galya.mik16@gmail.com>
2021-09-19 18:03:32 +0200

    Added DOM manipulation for new feedback from fan

---
+++ js/fans.js
@@ -0,0 +1,18 @@
+const new_post = document.getElementById("new-feedback");
+const text_area = document.getElementById("text-area");
+
+function submit_feedback() {
+    if(text_area.value !== "") {
+        new_post.innerHTML = `<article class="fans">
+            <p>${text_area.value}</p>
+            <div class="fan-article-footer">
+                <div class="fan-info">${new Date().toUTCString()}</div>
+                <div class="fan-info">Fan #1</div>
+            </div>
+            <hr />
+        </article>`;
+        text_area.value = "";
+    } else {
+        alert("Textarea can't be blank!")
+    }
+}
